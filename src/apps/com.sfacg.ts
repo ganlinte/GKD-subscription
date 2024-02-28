a91b5f1ae46d0dd7df0c93ccd39b8c4d2329e95e
ganlinte <weisongma03@163.com>
2024-02-28 09:34:32 +0800

    fix(菠萝包轻小说): 设置开屏广告规则运行时间，防误触

--- src/apps/com.sfacg.ts
+++ src/apps/com.sfacg.ts
@@ -7,6 +7,10 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
+      matchTime: 10000,
+      quickFind: true,
+      resetMatch: 'app',
+      actionMaximum: 1,
       activityIds: 'com.sf.ui.launcher.LauncherActivity',
       rules: 'TextView[text*="跳过"]',
       snapshotUrls: 'https://i.gkd.li/import/14320791',
