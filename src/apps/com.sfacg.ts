e3c59149b6f7600482320f4fe317875f9d04574a
ganlinte <weisongma03@163.com>
2024-02-28 09:43:52 +0800

    feat(菠萝包轻小说): 新增新春小说折扣季规则

--- src/apps/com.sfacg.ts
+++ src/apps/com.sfacg.ts
@@ -15,5 +15,16 @@ export default defineAppConfig({
       rules: 'TextView[text*="跳过"]',
       snapshotUrls: 'https://i.gkd.li/import/14320791',
     },
+    {
+      key: 1,
+      name: '局部广告-新春小说折扣季',
+      matchTime: 10000,
+      quickFind: true,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      activityIds: 'com.sf.ui.main.MainActivity',
+      rules: 'ImageView[id="com.sfacg:id/imgClose"]',
+      snapshotUrls: 'https://i.gkd.li/import/14427490',
+    },
   ],
 });
