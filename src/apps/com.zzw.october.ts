f6992c884d41632fcabd900e60f3010eb93a49d3
ganlinte <weisongma03@163.com>
2024-03-20 11:10:23 +0800

    feat(志愿汇): 添加[全屏广告-我的]

--- src/apps/com.zzw.october.ts
+++ src/apps/com.zzw.october.ts
@@ -5,6 +5,20 @@ export default defineAppConfig({
   name: '志愿汇',
   groups: [
     {
+      key: 1,
+      name: '全屏广告-我的',
+      quickFind: true,
+      actionMaximum: 1,
+      rules: [
+        {
+          activityIds: 'com.zzw.october.MainActivity',
+          matches:
+            'FrameLayout[childCount=5] > FrameLayout > ImageView[text=null]',
+          snapshotUrls: 'https://i.gkd.li/import/14661569',
+        },
+      ],
+    },
+    {
       key: 2,
       name: '主页广告',
       activityIds: ['com.zzw.october.MainActivity'],
