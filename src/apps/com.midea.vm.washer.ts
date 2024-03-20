7447216623cd1d54e05feec883f5ee236ee947fe
ganlinte <weisongma03@163.com>
2024-03-20 11:08:24 +0800

    feat(U净): 添加[全屏广告-首页]

--- src/apps/com.midea.vm.washer.ts
+++ src/apps/com.midea.vm.washer.ts
@@ -14,5 +14,20 @@ export default defineAppConfig({
       rules: '[text*="跳过"][text.length<=10]',
       snapshotUrls: 'https://i.gkd.li/import/13407199',
     },
+    {
+      key: 1,
+      name: '全屏广告-首页',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      rules: [
+        {
+          name: '领劵',
+          activityIds: 'com.midea.vm.washer.ui.activity.IndexActivity',
+          matches: 'ImageView[id="com.midea.vm.washer:id/close_img"]',
+          snapshotUrls: 'https://i.gkd.li/import/14661498',
+        },
+      ],
+    },
   ],
 });
