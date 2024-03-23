fcde4780f2ea5188f3f729f1c57bc44776d4b6d6
ganlinte <weisongma03@163.com>
2024-03-23 16:57:39 +0800

    perf(胖乖生活): 优化[开屏广告]

--- src/apps/com.qiekj.user.ts
+++ src/apps/com.qiekj.user.ts
@@ -8,22 +8,19 @@ export default defineAppConfig({
       key: 0,
       name: '开屏广告',
       activityIds: 'com.qiekj.user.MainActivity',
-
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      resetMatch: 'app',
       rules: [
         {
-          key: 0,
-          matches:
-            '[id="com.qiekj.user:id/native_splash_view"] >n [text^="跳过"]',
-          snapshotUrls: 'https://i.gkd.li/import/12903088',
+          matches: 'TextView[text*="跳过"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12903088',
+            'https://i.gkd.li/import/12903086',
+          ],
         },
         {
-          key: 1,
-          matches:
-            '[id="com.kwad.dy.sdk:id/ksad_splash_root_container"] >n [text="跳过"]',
-          snapshotUrls: 'https://i.gkd.li/import/12903086',
-        },
-        {
-          key: 2,
           matches:
             '[id="com.kwad.dy.sdk:id/ksad_splash_root_container"] >n ViewGroup > TextView + TextView + ImageView[clickable=true]',
           snapshotUrls: 'https://i.gkd.li/import/12903095',
