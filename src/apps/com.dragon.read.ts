8d0a175528fe716e656c40822003efe8d920f04d
ganlinte <weisongma03@163.com>
2024-03-31 12:30:49 +0800

    refactor(番茄免费小说): 重构

--- src/apps/com.dragon.read.ts
+++ src/apps/com.dragon.read.ts
@@ -3,56 +3,21 @@ import { defineAppConfig } from '../types';
 export default defineAppConfig({
   id: 'com.dragon.read',
   name: '番茄免费小说',
-  deprecatedKeys: [-1, 11],
   groups: [
     {
-      key: 0,
-      name: '卡片式广告',
-      activityIds: [
-        'com.dragon.read.ad.banner.ui',
-        'com.dragon.read.reader.ReaderActivity',
-        'com.dragon.read.reader.ui.ReaderActivity',
-      ],
-      rules: [
-        {
-          key: 0,
-          matches: '@[clickable=true] TextView[text="关闭此条广告"]',
-        },
-        {
-          key: 1,
-          matches: '@ImageView - LinearLayout TextView[text="广告"]',
-          snapshotUrls: 'https://i.gkd.li/import/12908734',
-        },
-        {
-          key: 2,
-          matches:
-            'FrameLayout > FrameLayout > ViewGroup[childCount=4] > @FrameLayout[clickable=true][visibleToUser=true] > ImageView',
-          snapshotUrls: [
-            'https://i.gkd.li/import/12716444',
-            'https://i.gkd.li/import/13062909', // 误触
-          ],
-        },
-        {
-          key: 3,
-          quickFind: true,
-          matches:
-            '[id="com.dragon.read:id/layout_banner_ad_bg"] > [id="com.dragon.read:id/close_button"]',
-          snapshotUrls: 'https://i.gkd.li/import/13520314',
-        },
-      ],
-    },
-    {
       key: 1,
-      name: '更新弹窗',
+      name: '更新提示',
       actionMaximum: 1,
+      matchTime: 20000,
       resetMatch: 'app',
       quickFind: true,
+      activityIds: 'com.dragon.read.update.b',
       rules: '@[text="以后再说"] + [text="优先体验"]',
       snapshotUrls: 'https://i.gkd.li/import/12716477',
     },
     {
       key: 2,
-      name: '首页右侧悬浮广告',
+      name: '局部广告-书城-右侧悬浮红包广告',
       activityIds: [
         'com.dragon.read.pages.main.MainFragmentActivity',
         'com.dragon.read.ad.openingscreenad.OpeningScreenADActivity',
@@ -70,7 +35,7 @@ export default defineAppConfig({
     },
     {
       key: 3,
-      name: '优惠券弹窗',
+      name: '全屏广告-书城-优惠券弹窗',
       rules: [
         {
           key: 0,
@@ -94,20 +59,83 @@ export default defineAppConfig({
     },
     {
       key: 4,
-      name: '阅读页面_关注作者',
+      name: '全屏广告-书城-周末开红包',
       quickFind: true,
+      matchTime: 20000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.dragon.read.pages.main.MainFragmentActivity',
       rules: [
         {
-          key: 0,
-          activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
-          matches: '@ImageView +2 FrameLayout >3 [text="关注"]',
-          snapshotUrls: 'https://i.gkd.li/import/13399505',
+          matches:
+            'FlattenUIText[text*="开心收下"] +n FlattenUIImage[clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/import/14383684',
         },
       ],
     },
     {
       key: 5,
-      name: '分段广告',
+      name: '全屏广告-书城-动画小说',
+      quickFind: true,
+      matchTime: 20000,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      activityIds: 'com.dragon.read.pages.main.MainFragmentActivity',
+      rules: [
+        {
+          matches: '@ImageView - RelativeLayout < LinearLayout[childCount=2]',
+          snapshotUrls: 'https://i.gkd.li/import/14813934',
+        },
+      ],
+    },
+    {
+      key: 6,
+      name: '全屏广告-书城-开启推送提醒',
+      desc: '自动点击【取消】',
+      enable: false,
+      activityIds: 'com.dragon.read.widget.ConfirmDialogBuilder',
+      rules: '@[text="取消"] < * -2 * > [text="开启推送提醒"]',
+      snapshotUrls: 'https://i.gkd.li/import/12716592',
+    },
+    {
+      key: 11,
+      name: '局部广告-阅读中卡片广告',
+      enable: false,
+      quickFind: true,
+      activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
+      rules: [
+        {
+          matches: '@ImageView - LinearLayout >n TextView[text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/import/12908734',
+        },
+        {
+          matches:
+            'ViewGroup[childCount=4] > FrameLayout[id!=null][clickable=true]',
+          snapshotUrls: 'https://i.gkd.li/import/12716444',
+        },
+        {
+          matches: 'ImageView[id="com.dragon.read:id/close_button"]',
+          snapshotUrls: 'https://i.gkd.li/import/13520314',
+        },
+        {
+          name: '卡片-关注作者',
+          matches: 'TextView[text="作者"] + ImageView',
+          snapshotUrls: 'https://i.gkd.li/import/13399505',
+        },
+      ],
+    },
+    {
+      key: 12,
+      name: '全屏广告-阅读中广告',
+      desc: '点击右上角【关闭】',
+      quickFind: true,
+      activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
+      rules: 'TextView[text="广告"] +2 Button[id="com.dragon.read:id/close"]',
+      snapshotUrls: 'https://i.gkd.li/import/13191156',
+    },
+    {
+      key: 13,
+      name: '全屏广告-阅读中视频广告',
       quickFind: true,
       rules: [
         {
@@ -158,36 +186,40 @@ export default defineAppConfig({
       ],
     },
     {
+      key: 99,
+      name: '局部广告-卡片广告（废弃）',
+      desc: '原卡片广告，重构后暂时废弃，新的卡片广告没问题,则删除此规则',
       enable: false,
-      key: 10,
-      name: '请求通知权限弹窗',
-      desc: '自动点击【取消】',
-      activityIds: 'com.dragon.read.widget.ConfirmDialogBuilder',
-      rules: '@[text="取消"] < * -2 * > [text="开启推送提醒"]',
-      snapshotUrls: 'https://i.gkd.li/import/12716592',
-    },
-    {
-      key: 12,
-      name: '关闭阅读-全屏广告',
-      desc: '点击右上角【关闭】',
-      quickFind: true,
-      activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
-      rules: 'TextView[text="广告"] +2 Button[id="com.dragon.read:id/close"]',
-      snapshotUrls: 'https://i.gkd.li/import/13191156',
-    },
-    {
-      key: 13,
-      name: '全屏广告-周末开红包',
-      quickFind: true,
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      activityIds: 'com.dragon.read.pages.main.MainFragmentActivity',
+      activityIds: [
+        'com.dragon.read.ad.banner.ui',
+        'com.dragon.read.reader.ReaderActivity',
+        'com.dragon.read.reader.ui.ReaderActivity',
+      ],
       rules: [
         {
+          key: 0,
+          matches: '@[clickable=true] TextView[text="关闭此条广告"]',
+        },
+        {
+          key: 1,
+          matches: '@ImageView - LinearLayout TextView[text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/import/12908734',
+        },
+        {
+          key: 2,
           matches:
-            'FlattenUIText[text*="开心收下"] +n FlattenUIImage[clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/import/14383684',
+            'FrameLayout > FrameLayout > ViewGroup[childCount=4] > @FrameLayout[clickable=true][visibleToUser=true] > ImageView',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12716444',
+            'https://i.gkd.li/import/13062909', // 误触
+          ],
+        },
+        {
+          key: 3,
+          quickFind: true,
+          matches:
+            '[id="com.dragon.read:id/layout_banner_ad_bg"] > [id="com.dragon.read:id/close_button"]',
+          snapshotUrls: 'https://i.gkd.li/import/13520314',
         },
       ],
     },
