37c18f62284bb8a9452d9b5777941844a776a494
ganlinte <weisongma03@163.com>
2024-04-07 18:28:43 +0800

    feat(我的荣耀): 新增 `开屏广告`

--- src/apps/com.hihonor.phoneservice.ts
+++ src/apps/com.hihonor.phoneservice.ts
@@ -5,6 +5,21 @@ export default defineAppConfig({
   name: '我的荣耀',
   groups: [
     {
+      key: 0,
+      name: '开屏广告',
+      quickFind: true,
+      matchTime: 10000,
+      actionMaximum: 1,
+      rules: [
+        {
+          activityIds: 'com.hihonor.phoneservice.OpenScreenAdActivity',
+          matches:
+            'TextView[id="com.hihonor.phoneservice:id/start_skip_count_down"]',
+          snapshotUrls: 'https://i.gkd.li/import/14896804',
+        },
+      ],
+    },
+    {
       enable: false,
       key: 10,
       name: '请求位置权限提示信息',
