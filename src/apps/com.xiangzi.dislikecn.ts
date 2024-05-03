0fb5be57328fd72321b1cb8ac934780a73aa9ccb
ganlinte <techenergymws@163.com>
2024-05-03 15:38:29 +0800

    feat(Dislike): 新增 `开屏广告` , fixes #4

---
+++ src/apps/com.xiangzi.dislikecn.ts
@@ -0,0 +1,25 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.xiangzi.dislikecn',
+  name: 'Dislike',
+  groups: [
+    {
+      key: 0,
+      name: '开屏广告',
+      desc: '点击 跳过',
+      matchTime: 10000,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      quickFind: true,
+      rules: [
+        {
+          activityIds: 'com.xiangzi.dislike.activity.SplashActivity',
+          matches:
+            '@View <2 FrameLayout[childCount=3] <2 FrameLayout[childCount=2] < [vid="csj_ad_container"]',
+          snapshotUrls: 'https://i.gkd.li/import/15196702',
+        },
+      ],
+    },
+  ],
+});
