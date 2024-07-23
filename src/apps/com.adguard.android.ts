7f08cd96cfff38429d2c3c53cd77c1a20c0d0da4
ganlinte <techenergymws@163.com>
2024-07-23 18:35:07 +0800

    feat(Adguard):新增`功能类-自动开启保护`

---
+++ src/apps/com.adguard.android.ts
@@ -0,0 +1,16 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+    id: 'com.adguard.android',
+    name: 'AdGuard',
+    groups: [
+        {
+            key: 1,
+            name: '功能类-自动开启保护',
+            quickFind: true,
+            actionMaximum: 1,
+            rules: '[id="com.adguard.android:id/main_switch"][text="主保护开关已关闭"]',
+            snapshotUrls: 'https://i.gkd.li/import/16346724'
+        }
+    ]
+});
