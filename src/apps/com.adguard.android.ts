70495f3e89343a8d703eecdba6a1349afc8391f9
ganlinte <techenergymws@163.com>
2024-09-27 12:32:11 +0800

    fix(Adguard): 修复自动开启保护多次点击

--- src/apps/com.adguard.android.ts
+++ src/apps/com.adguard.android.ts
@@ -9,6 +9,7 @@ export default defineGkdApp({
       name: '功能类-自动开启保护',
       quickFind: true,
       actionMaximum: 1,
+      resetMatch: 'app',
       rules:
         '[id="com.adguard.android:id/main_switch"][text="主保护开关已关闭"]',
       snapshotUrls: 'https://i.gkd.li/import/16346724',
