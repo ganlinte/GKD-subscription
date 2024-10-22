3bb9bcd10a038fe78191df987cbc58174cf6c198
ganlinte <techenergymws@163.com>
2024-10-22 18:53:41 +0800

    pref(番茄免费小说):查询优化

--- src/apps/com.dragon.read.ts
+++ src/apps/com.dragon.read.ts
@@ -139,11 +139,107 @@ export default defineGkdApp({
       name: '局部广告-阅读中卡片广告',
       desc: '点击X',
       fastQuery: true,
+      matchRoot: true,
       activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
       rules: [
         {
           matches:
-            '@ImageView - LinearLayout[childCount=3] > FrameLayout[childCount=1] > TextView[text!=""]',
+            '@ImageView - LinearLayout <n * <n * <n * <n * <n * <n * <n * <n * <n * <n [id="com.dragon.read:id/root_view"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12908734',
+            'https://i.gkd.li/import/13520314', //有唯一ID
+            'https://i.gkd.li/import/15294350',
+            'https://i.gkd.li/i/17474877',
+          ],
+        },
+        {
+          matches:
+            '@ImageView < FrameLayout <3 ViewGroup[childCount=4] <n * <n * <n * <n * <n * <n  * <n * <n * <n * <n * <n * <n [id="com.dragon.read:id/root_view" || id="android:id/content"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/12716444',
+            'https://i.gkd.li/i/17474881',
+          ]
+        },
+        {
+          name: '卡片-关注作者',
+          matches: '@ImageView + TextView[text="关注我，掌握书籍最新动态"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13399505',
+            'https://i.gkd.li/import/14896809',
+          ],
+        },
+      ],
+    },
+    {
+      key: 12,
+      name: '全屏广告-阅读中广告',
+      desc: '点击右上角【关闭】',
+      fastQuery: true,
+      activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
+      rules: [
+        {
+          matches: '@[id="com.dragon.read:id/close"] -2 [text="广告"]',
+          snapshotUrls: 'https://i.gkd.li/import/13191156',
+        },
+        {
+          matchRoot: true,
+          excludeMatches: '@[text="反馈"][visibleToUser=true]',
+          matches: '[text="点击关闭广告并退出小说"][visibleToUser=true]',
+          position: { left: '1100 - left -100', top: '2400 - top -300' },
+          snapshotUrls: [
+            'https://i.gkd.li/i/17474887',
+            'https://i.gkd.li/i/17474889',
+          ]
+        },
+        {
+          name: '领金币',
+          matches: '@ImageView < [id="com.dragon.read:id/parent"]',
+          snapshotUrls: 'https://i.gkd.li/i/17474890',
+        },
+      ],
+    },
+    {
+      key: 13,
+      name: '分段广告-阅读中视频广告',
+      fastQuery: true,
+      activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
+      rules: [
+        {
+          key: 0,
+          name: '点击反馈按钮',
+          matches: '@[text="反馈"][visibleToUser=true]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13520160',
+            'https://i.gkd.li/i/17468490',
+            'https://i.gkd.li/i/17474896',
+          ],
+        },
+        {
+          preKeys: 0,
+          name: '点击不感兴趣1',
+          matches: '@[text="不感兴趣"] <2 * + ViewGroup > [text="举报"]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/13520219',
+            'https://i.gkd.li/import/13674550',
+          ],
+        },
+        {
+          preKeys: 0,
+          name: '点击不感兴趣2',
+          matches: '@[text="不感兴趣"] + [text="举报"]',
+          snapshotUrls: 'https://i.gkd.li/i/17461153',
+        },
+      ],
+    },
+    {
+      key: 14,
+      name: '局部广告-阅读中卡片广告(旧)',
+      desc: '优化前旧版本',
+      fastQuery: true,
+      activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
+      rules: [
+        {
+          matches: '@ImageView - LinearLayout[childCount=3] > FrameLayout[childCount=1] > TextView[text!=""]',
           snapshotUrls: [
             'https://i.gkd.li/import/12908734',
             'https://i.gkd.li/import/13520314', //有唯一ID
@@ -164,66 +260,6 @@ export default defineGkdApp({
           ],
         },
       ],
-    },
-    {
-      key: 12,
-      name: '全屏广告-阅读中广告',
-      desc: '点击右上角【关闭】',
-      fastQuery: true,
-      activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
-      rules: 'TextView[text="广告"] +2 Button[id="com.dragon.read:id/close"]',
-      snapshotUrls: 'https://i.gkd.li/import/13191156',
-    },
-    {
-      key: 13,
-      name: '分段广告-阅读中视频广告',
-      fastQuery: true,
-      rules: [
-        {
-          key: 0,
-          name: '点击反馈按钮',
-          activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
-          // 有反馈原规则'[text="反馈"][clickable=true]'不触发删除[clickable=true]才能点击；有反馈原规则点击屏外节点，导致无法执行下一步，遂添加[visibleToUser=true]
-          matches: '[text="反馈"][visibleToUser=true]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/13520160',
-            'https://i.gkd.li/import/13843155',
-          ],
-        },
-        {
-          preKeys: 0,
-          key: 1,
-          name: '点击不感兴趣',
-          activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
-          matches: '@ViewGroup[clickable=true] > [text="不感兴趣"]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/13520219',
-            'https://i.gkd.li/import/13674550',
-          ],
-        },
-        {
-          key: 2,
-          name: '点击下一页',
-          activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
-          matches:
-            '[id="com.dragon.read:id/readFlowNonRoundEntranceLayout"] [id="com.dragon.read:id/relativeRight"]',
-          snapshotUrls: 'https://i.gkd.li/import/13674556',
-        },
-        {
-          key: 3,
-          name: '点击反馈',
-          activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
-          matches: '[text="看视频免广告"] - [text="反馈"]',
-          snapshotUrls: 'https://i.gkd.li/import/13816453',
-        },
-        {
-          preKeys: 3,
-          key: 4,
-          activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
-          matches: '[text="举报"] <2 ViewGroup - ViewGroup[clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/import/13816454',
-        },
-      ],
-    },
+    }
   ],
 });
