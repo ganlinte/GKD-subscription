f97b1f1d902af17686833a3b28d9250b31707de4
ganlinte <techenergymws@163.com>
2024-11-18 09:21:58 +0800

    fix(胖乖生活):移除`开屏广告`,修复`全屏广告-弹窗`,移除`全屏广告-我的-开通胖乖会员`

--- src/apps/com.qiekj.user.ts
+++ src/apps/com.qiekj.user.ts
@@ -5,39 +5,15 @@ export default defineGkdApp({
   name: '胖乖生活',
   groups: [
     {
-      key: 0,
-      name: '开屏广告',
-      activityIds: 'com.qiekj.user.MainActivity',
-      quickFind: true,
-      matchTime: 10000,
-      actionMaximum: 1,
-      resetMatch: 'app',
-      rules: [
-        {
-          matches: 'TextView[text*="跳过"]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/12903088',
-            'https://i.gkd.li/import/12903086',
-          ],
-        },
-        {
-          matches:
-            '[id="com.kwad.dy.sdk:id/ksad_splash_root_container"] >n ViewGroup > TextView + TextView + ImageView[clickable=true]',
-          snapshotUrls: 'https://i.gkd.li/import/12903095',
-        },
-      ],
-    },
-    {
-      key: 2,
+      key: 1,
       name: '更新提示',
       activityIds: 'com.qiekj.user.MainActivity',
-      quickFind: true,
+      fastQuery: true,
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'app',
       rules: [
         {
-          key: 0,
           matches:
             '[id="com.qiekj.user:id/btn_cancelUpdate"][text*="暂不更新"]',
           snapshotUrls: 'https://i.gkd.li/import/13435011',
@@ -45,72 +21,23 @@ export default defineGkdApp({
       ],
     },
     {
-      key: 10,
-      name: '全屏广告-我的-弹窗广告',
-      enable: false,
-      quickFind: true,
+      key: 2,
+      name: '全屏广告-弹窗',
+      desc: '点击【X】/【跳过】',
+      fastQuery: true,
+      activityIds: 'com.sigmob.sdk.base.common.PortraitTransparentAdActivity',
       rules: [
         {
-          key: 0,
-          activityIds:
-            'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
-          matches: 'TextView[text*="反馈"] -n View > Image[text.length=0]',
-          snapshotUrls: 'https://i.gkd.li/import/14304573',
-        },
-        {
-          key: 1,
-          activityIds: 'com.qiekj.user.MainActivity',
-          matches:
-            'ViewGroup[childCount>=5] > ViewGroup[childCount=1] > ViewGroup[clickable=true]',
-          snapshotUrls: [
-            'https://i.gkd.li/import/14321282',
-            'https://i.gkd.li/import/14459980',
+          matches: [
+            '@TextView[text="广告"]',
+            '@TextView[id="close_btn"]',
           ],
-        },
-        {
-          activityIds: 'com.qiekj.user.MainActivity',
-          matches: 'TextView[text*="关闭"]',
-          snapshotUrls: 'https://i.gkd.li/import/14460112',
-        },
-        {
-          activityIds: 'com.qiekj.user.MainActivity',
-          matches: 'FrameLayout[childCount=4] > FrameLayout > ImageView',
-          snapshotUrls: 'https://i.gkd.li/import/14467391',
-        },
-        {
-          key: 2,
-          activityIds:
-            'com.sigmob.sdk.base.common.PortraitTransparentAdActivity',
-          matches: 'TextView[text*="跳过"]',
-          snapshotUrls: 'https://i.gkd.li/import/14383463',
-        },
-        {
-          preKeys: 2,
-          key: 3,
-          activityIds:
-            'com.sigmob.sdk.base.common.PortraitTransparentAdActivity',
-          matches: 'TextView[id="close_btn"]',
-          snapshotUrls: 'https://i.gkd.li/import/14383477',
-        },
-        {
-          key: 4,
-          activityIds: 'com.baidu.mobads.sdk.api.MobRewardVideoActivity',
-          matches: 'RelativeLayout[childCount=3||childCount=4] + ImageView',
           snapshotUrls: [
-            'https://i.gkd.li/import/14545261',
-            'https://i.gkd.li/import/14545318',
+            'https://i.gkd.li/i/17789291',
+            'https://i.gkd.li/i/17789294',
           ],
         },
       ],
-    },
-    {
-      key: 11,
-      name: '全屏广告-我的-开通胖乖会员',
-      enable: false,
-      quickFind: true,
-      activityIds: 'com.qiekj.user.MainActivity',
-      rules: 'TextView[text="暂不开通"]',
-      snapshotUrls: 'https://i.gkd.li/import/14460136',
     },
   ],
 });
