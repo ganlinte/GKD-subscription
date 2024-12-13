ffda41dfed88209bac1554895a15606bbacd609c
ganlinte <techenergymws@163.com>
2024-12-13 17:26:00 +0800

    fix(虎扑):修复`局部广告-推荐流广告`二段不触发

--- src/apps/com.hupu.games.ts
+++ src/apps/com.hupu.games.ts
@@ -6,17 +6,18 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
+      name: '局部广告-推荐流广告',
+      desc: '点击卡片右上角广告文字,出现广告反馈,点击屏蔽该广告',
+      fastQuery: true,
       activityIds: [
         'com.hupu.games.main.MainActivity',
         'com.hupu.android.bbs.detail.PostDetailActivity',
         'com.hupu.topic.TopicActivity',
       ],
-      quickFind: true,
-      name: '局部广告-推荐流广告',
-      desc: '点击卡片右上角广告文字,出现广告反馈,点击屏蔽该广告',
       rules: [
         {
-          key: 0,
+          key: 1,
+          name: '【广告】',
           matches:
             '@[id="com.hupu.games:id/shield_view"] >2 [id="com.hupu.games:id/tv_tag"][text="广告"]',
           snapshotUrls: [
@@ -26,14 +27,15 @@ export default defineGkdApp({
           ],
         },
         {
-          preKeys: 0,
-          key: 1,
-          matches:
-            '@[clickable=true] > [id="com.hupu.games:id/tv_title"][text="屏蔽该广告"]',
+          preKeys: 1,
+          key: 2,
+          name: '【屏蔽该广告/不感兴趣】',
+          matches: '@TextView[text="屏蔽该广告"||text="不感兴趣"]',
           snapshotUrls: [
-            'https://i.gkd.li/import/12511010',
-            'https://i.gkd.li/import/12534848',
-            'https://i.gkd.li/import/13259699',
+            'https://i.gkd.li/import/12511010', //屏蔽该广告
+            'https://i.gkd.li/import/12534848', //屏蔽该广告
+            'https://i.gkd.li/import/13259699', //屏蔽该广告
+            'https://i.gkd.li/i/18123775' //不感兴趣
           ],
         },
       ],
