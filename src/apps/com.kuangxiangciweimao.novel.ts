65b612c7c0f0eb20ec868ab357c76a2222e1e2fb
ganlinte <techenergymws@163.com>
2025-01-29 20:39:45 +0800

    pref(刺猬猫阅读)：优化`自动签到`，延长匹配时间，防止与活动弹窗冲突

--- src/apps/com.kuangxiangciweimao.novel.ts
+++ src/apps/com.kuangxiangciweimao.novel.ts
@@ -78,7 +78,7 @@ export default defineGkdApp({
       resetMatch: 'app',
       actionMaximum: 1,
       activityIds: '.activity.frame.MainFrameActivity',
-      actionDelay: 500,
+      actionDelay: 2000,
       rules: [
         {
           key: 0,
@@ -89,7 +89,7 @@ export default defineGkdApp({
         {
           preKeys: 0,
           action: 'clickCenter',
-          actionDelay: 200,
+          actionDelay: 500,
           matches:
             '[id="com.kuangxiangciweimao.novel:id/signBtn"][text="每周福利"]',
           snapshotUrls: 'https://i.gkd.li/i/17686396',
