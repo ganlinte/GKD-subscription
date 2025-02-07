4f9e349807ac6c870f987a649d4a8aafcdc9d8d9
ganlinte <techenergymws@163.com>
2025-02-07 16:53:49 +0800

    pref(全局规则):优化`开屏广告-全局`

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -8,23 +8,23 @@ export const YOUTH_MODE_ORDER = -8; // 青少年模式
 export default defineGkdGlobalGroups([
   {
     key: 0,
-    name: '开屏广告',
+    name: '开屏广告-全局',
     order: OPEN_AD_ORDER,
     matchTime: 10000,
     actionMaximum: 2,
+    fastQuery: true,
     resetMatch: 'app',
     actionCdKey: 0,
     actionMaximumKey: 0,
+    priorityTime: 10000,
     rules: [
       {
         key: 0,
         fastQuery: true,
-        matches: '[text*="跳过"][text.length<10][visibleToUser=true]',
-      },
-      {
-        key: 1,
-        matches:
+        anyMatches: [
+          '[text*="跳过"][text.length<10][visibleToUser=true]',
           '[childCount=0][visibleToUser=true][((text*="跳过" || text*="跳過" || text~="(?is).*skip.*") && text.length<10) || id~="(?is).*tt_splash_skip_btn" || vid~="(?is).*skip.*" || (vid~="(?is).*count.*" && vid~="(?is).*down.*" && vid!~="(?is).*load.*" && vid!~="(?is).*time.*" && vid!~="(?is).*hour.*" && vid!~="(?is).*minute.*" && vid!~="(?is).*second.*" && vid!~="(?is).*add.*" && vid!~="(?is).*ead.*") || ((desc*="跳过" || desc*="跳過" || desc~="(?is).*skip.*") && desc.length<10)]',
+        ]
       },
     ],
     apps: [...appList.openAdBlackListAppIDs]
