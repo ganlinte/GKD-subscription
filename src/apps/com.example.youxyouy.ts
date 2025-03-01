2f2781b5ba9348e40494f2f538433dd47d069bcb
ganlinte <techenergymws@163.com>
2025-03-01 10:19:40 +0800

    feat(有鱼生活): 新增`全屏广告-首页广告弹窗`(fixes #154)

--- src/apps/com.example.youxyouy.ts
+++ src/apps/com.example.youxyouy.ts
@@ -6,18 +6,20 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '全屏广告-贡献金弹窗',
+      name: '全屏广告-广告弹窗',
       fastQuery: true,
       activityIds: '.home.RewardVideoAdActivity',
       rules: [
         {
           anyMatches: [
-            '[id="com.example.youxyouy:id/iv_cancel"]',
+            'ImageView[id="com.example.youxyouy:id/iv_cancel"]',
+            'ImageView[id="com.example.youxyouy:id/iv_close"]',
             '[id="com.example.youxyouy:id/tv_exit"][text="退出"]',
             '[id="com.example.youxyouy:id/tv_action"][text="确定"]',
           ],
           snapshotUrls: [
             'https://i.gkd.li/i/18878136',
+            'https://i.gkd.li/i/18929693',
             'https://i.gkd.li/i/18878152',
             'https://i.gkd.li/i/18878154',
           ],
@@ -34,6 +36,21 @@ export default defineGkdApp({
         {
           matches: '[id="com.example.youxyouy:id/iv_replay"][text="跳过"]',
           snapshotUrls: 'https://i.gkd.li/i/18878101',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '全屏广告-首页广告弹窗',
+      fastQuery: true,
+      activityIds: '.MainActivity',
+      rules: [
+        {
+          matches: 'ImageView[id="com.example.youxyouy:id/iv_close"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/19019416',
+            'https://i.gkd.li/i/19019419',
+          ],
         },
       ],
     },
