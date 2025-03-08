a073e5cb77ed6a8ef0dddde48c1edd67e9e36e3b
ganlinte <techenergymws@163.com>
2025-03-08 15:58:08 +0800

    feat(有鱼生活): 新增广告规则，优化规则分类 (fixes #159)

--- src/apps/com.example.youxyouy.ts
+++ src/apps/com.example.youxyouy.ts
@@ -6,7 +6,7 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '全屏广告-广告弹窗',
+      name: '全屏广告-视频广告弹窗',
       fastQuery: true,
       activityIds: '.home.RewardVideoAdActivity',
       rules: [
@@ -16,26 +16,44 @@ export default defineGkdApp({
             'ImageView[id="com.example.youxyouy:id/iv_close"]',
             '[id="com.example.youxyouy:id/tv_exit"][text="退出"]',
             '[id="com.example.youxyouy:id/tv_action"][text="确定"]',
+            '[id="com.example.youxyouy:id/iv_replay"][text="跳过"]',
           ],
           snapshotUrls: [
             'https://i.gkd.li/i/18878136',
             'https://i.gkd.li/i/18929693',
             'https://i.gkd.li/i/18878152',
             'https://i.gkd.li/i/18878154',
+            'https://i.gkd.li/i/18878101',
           ],
         },
       ],
     },
     {
       key: 2,
-      name: '全屏广告-收获更多奖励',
-      desc: '【跳过】',
+      name: '全屏广告-SDK广告',
       fastQuery: true,
-      activityIds: '.home.RewardVideoAdActivity',
       rules: [
         {
-          matches: '[id="com.example.youxyouy:id/iv_replay"][text="跳过"]',
-          snapshotUrls: 'https://i.gkd.li/i/18878101',
+          activityIds: 'com.bytedance.sdk.openadsdk.stub.activity.Stub_Standard_Portrait_Activity',
+          anyMatches: [
+            'TextView[text="跳过"]',
+            '@ImageView < LinearLayout < LinearLayout - FrameLayout > TextView[text="反馈"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/19117630',
+            'https://i.gkd.li/i/19117646',
+          ],
+        },
+        {
+          activityIds: 'com.kwad.sdk.api.proxy.app.KsRewardVideoActivity',
+          anyMatches: [
+            'TextView[text="继续观看"]',
+            '@ImageView < ViewGroup < ViewGroup < ViewGroup[desc="close_button"] - ViewGroup > ViewGroup > TextView[text="立即获取"]',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/19117637',
+            'https://i.gkd.li/i/19117609',
+          ],
         },
       ],
     },
