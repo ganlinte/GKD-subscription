7a1165e41cd4e91e1319adda205cea8fe5f77763
ganlinte <techenergymws@163.com>
2025-03-13 12:55:51 +0800

    feat(应用市场): 新增`全屏广告-为您专属推荐`,`全屏广告-应用推荐卡片添加至桌面`,`全屏广告-个性化内容推荐`,`通知提示`

--- src/apps/com.hihonor.appmarket.ts
+++ src/apps/com.hihonor.appmarket.ts
@@ -8,7 +8,7 @@ export default defineGkdApp({
       key: 0,
       name: '开屏广告',
       desc: '点击 跳过',
-      quickFind: true,
+      fastQuery: true,
       activityIds: 'com.hihonor.appmarket.module.main.MainActivity',
       actionMaximum: 1,
       matchTime: 10000,
@@ -22,8 +22,7 @@ export default defineGkdApp({
     {
       key: 1,
       name: '全屏广告-广告提示',
-      quickFind: true,
-
+      fastQuery: true,
       activityIds: [
         'com.hihonor.android.launcher.unihome.UniHomeLauncher',
         'com.hihonor.appmarket.module.main.MainActivity',
@@ -37,7 +36,7 @@ export default defineGkdApp({
     {
       key: 2,
       name: '局部广告-悬浮窗小广告',
-      quickFind: true,
+      fastQuery: true,
       activityIds: 'com.hihonor.appmarket.module.main.MainActivity',
       rules: '[id="com.hihonor.appmarket:id/iv_floating_close"]',
       snapshotUrls: 'https://i.gkd.li/import/13063928',
@@ -45,11 +44,75 @@ export default defineGkdApp({
     {
       key: 3,
       name: '全屏广告-推送通知',
-      quickFind: true,
+      fastQuery: true,
       activityIds: 'com.hihonor.appmarket.module.main.MainActivity',
       rules:
         'TextView[text="接收通知"] < LinearLayout < LinearLayout +n [id="android:id/buttonPanel"] Button[text="否"]',
       snapshotUrls: 'https://i.gkd.li/import/13073319',
+    },
+    {
+      key: 4,
+      name: '全屏广告-为您专属推荐',
+      desc: '【取消】',
+      fastQuery: true,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      matchTime: 10000,
+      activityIds: '.module.main.MainActivity',
+      rules: [
+        {
+          matches: 'Button[id="com.hihonor.appmarket:id/app_recommendation_cancel_btn"][text="取消"]',
+          snapshotUrls: 'https://i.gkd.li/i/19222443',
+        }
+      ],
+    },
+    {
+      key: 5,
+      name: '全屏广告-应用推荐卡片添加至桌面',
+      desc: '【我再想想】',
+      fastQuery: true,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      matchTime: 10000,
+      activityIds: '.module.main.MainActivity',
+      rules: [
+        {
+          matches: 'Button[id="com.hihonor.appmarket:id/app_cancel_btn"][text="我再想想"]',
+          snapshotUrls: 'https://i.gkd.li/i/19222473',
+        }
+      ],
+    },
+    {
+      key: 6,
+      name: '全屏广告-个性化内容推荐',
+      desc: '【取消】',
+      fastQuery: true,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      matchTime: 10000,
+      activityIds: '.module.main.MainActivity',
+      rules: [
+        {
+          matches: 'Button[id="android:id/button2"][text="取消"] + Button[id="android:id/button1"][text="同意"]',
+          snapshotUrls: 'https://i.gkd.li/i/19222508',
+        }
+      ],
+    },
+    {
+      key: 7,
+      name: '通知提示',
+      desc: '【取消】',
+      fastQuery: true,
+      resetMatch: 'app',
+      actionMaximum: 1,
+      matchTime: 10000,
+      activityIds: '.module.main.MainActivity',
+      rules: [
+        {
+          matches: '@Button[id="android:id/button2"][text="取消"] + Button[id="android:id/button1"][text="立即开启"]',
+          snapshotUrls: 'https://i.gkd.li/i/19222532',
+        }
+      ],
     },
   ],
 });
