b4d41a36f6ee3dccb82b0fb8e634b515540a2e3b
ganlinte <weisongma03@163.com>
2026-05-19 15:05:15 +0800

    feat(红果免费短剧): 新增翻页视频广告，滑动跳过 (fixes #368)

--- src/apps/com.phoenix.read.ts
+++ src/apps/com.phoenix.read.ts
@@ -18,5 +18,26 @@ export default defineGkdApp({
         },
       ],
     },
+    {
+      key: 2,
+      name: '全屏广告-阅读-视频广告',
+      desc: '滑动翻页',
+      fastQuery: true,
+      activityIds: 'com.dragon.read.reader.ui.ReaderActivity',
+      rules: [
+        {
+          matches: '[text="点击关闭广告并退出小说"]',
+          excludeMatches: '[text$="秒后，继续阅读下一页"]',
+          action: 'swipe',
+          swipeArg: {
+            start: { x: 'screenWidth*0.8', y: 'screenHeight/2' },
+            end: { x: 'screenWidth*0.2', y: 'screenHeight/2' },
+            duration: 500,
+          },
+          snapshotUrls: 'https://i.gkd.li/i/27988263',
+          excludeSnapshotUrls: 'https://i.gkd.li/i/27988163',
+        },
+      ],
+    },
   ],
 });
